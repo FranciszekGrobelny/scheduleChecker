a60d08eff0156caaedf91d570209c2deccd02712
franek <franciszekgrobelny@gmail.com>
2020-06-18 20:18:01 +0200

    initial commit

---
+++ src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
@@ -0,0 +1,13 @@
+package project.inz.scheduleChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/inz/scheduleChecker/ScheduleCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.inz.scheduleChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
