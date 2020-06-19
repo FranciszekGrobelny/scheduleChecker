92ce5f421014df47d4e0402017a9c38042a83500
franek <franciszekgrobelny@gmail.com>
2020-06-19 16:58:19 +0200

    app with index jsp view

--- src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
+++ src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
@@ -2,7 +2,6 @@ package project.inz.scheduleChecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import project.inz.scheduleChecker.domain.model.User;
 
 @SpringBootApplication
 public class ScheduleCheckerApplication {
