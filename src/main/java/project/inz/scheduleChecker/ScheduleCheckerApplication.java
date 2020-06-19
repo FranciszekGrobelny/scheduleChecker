1a5a844ce2404eedf7e84522be73dd7451f395f8
franek <franciszekgrobelny@gmail.com>
2020-06-19 10:58:25 +0200

    compiled app

--- src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
+++ src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
@@ -2,6 +2,7 @@ package project.inz.scheduleChecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import project.inz.scheduleChecker.domain.model.User;
 
 @SpringBootApplication
 public class ScheduleCheckerApplication {
@@ -9,5 +10,4 @@ public class ScheduleCheckerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScheduleCheckerApplication.class, args);
 	}
-
 }
