c8cf816b6491c26e9fc10e397a87f71d8855c7bc
franek <franciszekgrobelny@gmail.com>
2020-10-09 06:54:18 +0200

    add webMvcConfig and empty AppConfig

---
+++ src/main/java/project/inz/scheduleChecker/config/WebMvcConfig.java
@@ -0,0 +1,10 @@
+package project.inz.scheduleChecker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+
+}
