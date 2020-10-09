dc7ebf1fa42d8fcf3e8ca8e0f930f30fcb4bda48
franek <franciszekgrobelny@gmail.com>
2020-10-09 06:59:19 +0200

    add method getByName

--- src/main/java/project/inz/scheduleChecker/repository/TeacherRepository.java
+++ src/main/java/project/inz/scheduleChecker/repository/TeacherRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.inz.scheduleChecker.model.Teacher;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+    Teacher getTeacherByName(String name);
 }
