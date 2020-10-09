06128a30c9640f96cff3352de960f7a47fa92bd2
franek <franciszekgrobelny@gmail.com>
2020-10-09 07:09:01 +0200

    change addClass POST method

--- src/main/java/project/inz/scheduleChecker/controller/ClassController.java
+++ src/main/java/project/inz/scheduleChecker/controller/ClassController.java
@@ -1,15 +1,23 @@
 package project.inz.scheduleChecker.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import project.inz.scheduleChecker.dto.ClassDTO;
+import project.inz.scheduleChecker.dto.TopicWithHoursQuantityDTO;
+import project.inz.scheduleChecker.model.TopicWithHoursQuantity;
 import project.inz.scheduleChecker.service.ClassService;
 
-@Controller
+import javax.validation.Valid;
+
+@Controller @Slf4j
 public class ClassController {
 
     private final ClassService classService;
@@ -21,11 +29,16 @@ public class ClassController {
     @GetMapping("/addClass")
     public String addClass(Model model){
         model.addAttribute(new ClassDTO());
+        model.addAttribute(new TopicWithHoursQuantityDTO());
         return "/add/class.jsp";
     }
 
     @PostMapping("/addClass")
-    public String addClass(@ModelAttribute ClassDTO classDTO){
+    public String addClass(@ModelAttribute @Valid ClassDTO classDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            log.warn(bindingResult.toString());
+            return "/index.jsp";
+        }
         try{
             classService.save(classDTO);
             return "/add/class.jsp";
