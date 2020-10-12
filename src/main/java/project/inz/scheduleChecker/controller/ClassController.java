c3e31a3d43dbb54fb98b72d2c905e09a96f80ed0
franek <franciszekgrobelny@gmail.com>
2020-10-12 16:48:45 +0200

    add teachers  modelAttribute

--- src/main/java/project/inz/scheduleChecker/controller/ClassController.java
+++ src/main/java/project/inz/scheduleChecker/controller/ClassController.java
@@ -1,8 +1,6 @@
 package project.inz.scheduleChecker.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,18 +10,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import project.inz.scheduleChecker.dto.ClassDTO;
 import project.inz.scheduleChecker.dto.TopicWithHoursQuantityDTO;
-import project.inz.scheduleChecker.model.TopicWithHoursQuantity;
+import project.inz.scheduleChecker.model.Teacher;
 import project.inz.scheduleChecker.service.ClassService;
+import project.inz.scheduleChecker.service.TeacherService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller @Slf4j
 public class ClassController {
 
     private final ClassService classService;
+    private final TeacherService teacherService;
 
-    public ClassController(ClassService classService) {
+    public ClassController(ClassService classService, TeacherService teacherService) {
         this.classService = classService;
+        this.teacherService = teacherService;
+    }
+
+    @ModelAttribute("teachers")
+    public List<Teacher> teachers() {
+        return teacherService.findAll();
     }
 
     @GetMapping("/addClass")
