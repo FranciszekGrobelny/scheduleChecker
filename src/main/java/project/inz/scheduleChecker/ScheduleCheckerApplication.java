a14e7bd496adac5a0ff9ceeab90dea7b8604bbc8
franek <franciszekgrobelny@gmail.com>
2020-10-12 16:56:17 +0200

    no code files commit

--- src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
+++ src/main/java/project/inz/scheduleChecker/ScheduleCheckerApplication.java
@@ -2,7 +2,6 @@ package project.inz.scheduleChecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ScheduleCheckerApplication {
