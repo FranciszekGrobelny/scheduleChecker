bca7db0c0219da0b19910c3bff85c04c4b3bef6b
franek <franciszekgrobelny@gmail.com>
2020-10-12 16:43:43 +0200

    addFormater for TeacherConverter and ViewResolver

--- src/main/java/project/inz/scheduleChecker/config/AppConfig.java
+++ src/main/java/project/inz/scheduleChecker/config/AppConfig.java
@@ -2,10 +2,29 @@ package project.inz.scheduleChecker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import project.inz.scheduleChecker.repository.TeacherRepository;
-import project.inz.scheduleChecker.service.TeacherService;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import project.inz.scheduleChecker.converter.TeacherConverter;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver= new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views");
+//        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry formatterRegistry){
+        formatterRegistry.addConverter(getTeacherConverter());
+    }
+
+    @Bean
+    public TeacherConverter getTeacherConverter() {
+        return new TeacherConverter();
+    }
 }
