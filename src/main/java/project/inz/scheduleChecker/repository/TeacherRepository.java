9a9d38abe252c1bcd890e5e8f16e099f79371bd8
franek <franciszekgrobelny@gmail.com>
2020-10-12 14:02:28 +0200

    add method findById

--- src/main/java/project/inz/scheduleChecker/repository/TeacherRepository.java
+++ src/main/java/project/inz/scheduleChecker/repository/TeacherRepository.java
@@ -1,9 +1,11 @@
 package project.inz.scheduleChecker.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.inz.scheduleChecker.model.Teacher;
 
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
-    Teacher getTeacherByName(String name);
+    Teacher getTeacherById(Long Id);
 }
