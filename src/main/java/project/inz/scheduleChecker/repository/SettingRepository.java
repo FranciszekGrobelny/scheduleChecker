9fc5db6d7087c7748523c739e67c0d50c65feb3c
franek <franciszekgrobelny@gmail.com>
2020-10-14 20:59:52 +0200

    add findFirst method

--- src/main/java/project/inz/scheduleChecker/repository/SettingRepository.java
+++ src/main/java/project/inz/scheduleChecker/repository/SettingRepository.java
@@ -3,5 +3,8 @@ package project.inz.scheduleChecker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.inz.scheduleChecker.model.Setting;
 
-public interface SettingRepository extends JpaRepository<Setting, Long> {
+public interface SettingRepository extends JpaRepository<Setting, Long>, extendedSettingRepository {
+
+    void deleteAll();
+    Setting findFirst();
 }
