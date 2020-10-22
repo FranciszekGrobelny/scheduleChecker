b006d809e4511400ed607c31e93688da683794b5
franek <franciszekgrobelny@gmail.com>
2020-10-22 22:38:18 +0200

    add plan field

--- src/main/java/project/inz/scheduleChecker/model/Lesson.java
+++ src/main/java/project/inz/scheduleChecker/model/Lesson.java
@@ -4,11 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalTime;
+import java.util.Optional;
 
 @Entity
 @Table(name = "lessons")
@@ -34,4 +32,40 @@ public class Lesson extends ParentEntity {
     private Teacher teacher2;
 
     private String topic2;
+
+    @ManyToOne
+    private Plan plan;
+
+    public Lesson() {
+    }
+
+    public Lesson(boolean revalidationLesson,boolean connected,LocalTime startTime,LocalTime endTime,
+                  Teacher teacher,String topic,int room,Plan plan) {
+        this.revalidationLesson = revalidationLesson;
+        this.connected = connected;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.teacher = teacher;
+        this.topic = topic;
+        this.room = room;
+        this.plan = plan;
+    }
+
+    public Lesson(boolean revalidationLesson,boolean connected,LocalTime startTime,LocalTime endTime,
+                  Teacher teacher,String topic,int room,Teacher teacher2,String topic2,Plan plan) {
+        this.revalidationLesson = revalidationLesson;
+        this.connected = connected;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.teacher = teacher;
+        this.topic = topic;
+        this.room = room;
+        this.teacher2 = teacher2;
+        this.topic2 = topic2;
+        this.plan = plan;
+    }
+
+    public Lesson(boolean revalidationLesson, boolean connected, LocalTime start, LocalTime stop, Teacher teacherById, String religia, int room, Optional<Plan> plan) {
+        super();
+    }
 }
