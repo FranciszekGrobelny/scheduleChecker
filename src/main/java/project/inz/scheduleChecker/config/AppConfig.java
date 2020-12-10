3823d4296b1d84915f5b1c6c120c6d4eb46dc542
franek <franciszekgrobelny@gmail.com>
2020-12-10 21:42:29 +0100

    delete teacherConverter bean

--- src/main/java/project/inz/scheduleChecker/config/AppConfig.java
+++ src/main/java/project/inz/scheduleChecker/config/AppConfig.java
@@ -2,10 +2,8 @@ package project.inz.scheduleChecker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import project.inz.scheduleChecker.converter.TeacherConverter;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -14,17 +12,8 @@ public class AppConfig implements WebMvcConfigurer {
     public InternalResourceViewResolver jspViewResolver() {
         InternalResourceViewResolver resolver= new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/views");
-//        resolver.setSuffix(".jsp");
         return resolver;
     }
 
-    @Override
-    public void addFormatters(FormatterRegistry formatterRegistry){
-        formatterRegistry.addConverter(getTeacherConverter());
-    }
 
-    @Bean
-    public TeacherConverter getTeacherConverter() {
-        return new TeacherConverter();
-    }
 }
